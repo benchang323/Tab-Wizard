d0c671bc8260b97fc87072e8a5be41f80029cf21
Benjamin <benjaminch3230@gmail.com>
2022-01-17 11:08:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tab-Wizard
+ Code for the Safari extension Tab Wizard, aiming to produce the functions of saving tabs en masse and suspending tabs to save memory. 
---
+++ Tab Wizard/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Tab Wizard
+//
+//  Created by Benjamin Chang on 1/17/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
